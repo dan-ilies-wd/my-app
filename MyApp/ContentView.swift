1459ca0c1554fab8843f27c9b06bc2315c264b8b
Dan Ilies <dan.ilies@wolfpack-digital.com>
2023-03-29 16:04:16 +0300

    Change the globe color.

--- MyApp/ContentView.swift
+++ MyApp/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
+                .foregroundColor(.yellow)
             Text("Hello, Spring!")
         }
         .padding()
