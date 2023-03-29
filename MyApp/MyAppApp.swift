76419612c54d2e6d97aea9735f6dec4d48789aef
Dan Ilies <dan.ilies@wolfpack-digital.com>
2023-03-29 15:58:21 +0300

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Dan Ilies on 29.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
