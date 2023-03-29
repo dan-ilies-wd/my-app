b550167805caa3d506377d85ef6577ef1fb54fca
Dan Ilies <dan.ilies@wolfpack-digital.com>
2023-03-29 16:03:12 +0300

    Add a new message.

--- MyApp/ContentView.swift
+++ MyApp/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, Spring!")
+            Text("How are you today?") 
         }
         .padding()
     }
